298c07625c2de23d2096f8f62df16b90e2eddaab
kenpcjr <kennethcooke@me.com>
2016-08-08 13:46:40 -0400

    Moved getParks method to dataStore

--- Team-Tiger/Team-Tiger/DataStore.swift
+++ Team-Tiger/Team-Tiger/DataStore.swift
@@ -8,8 +8,12 @@
 
 import Foundation
 import CoreData
+import Alamofire
 
 class DataStore {
+    
+    var locationDictionary = [:]
+    var parsedDictionary = [String : [String : String]]()
     
     //static makes it a singleton
     static let store = DataStore()
@@ -37,6 +41,39 @@ class DataStore {
         fetchData()
 
     }
+    
+    
+    func getParks() {
+        
+        Alamofire.request(.GET, "https://data.cityofnewyork.us/api/views/p7jc-c8ak/rows.json?accessType=DOWNLOAD").responseJSON { (response) in
+            self.locationDictionary = response.result.value as! NSDictionary
+            
+            
+            let locationArrays = self.locationDictionary["data"] as! NSArray
+            
+            for location in locationArrays {
+                
+                var tempDictionary = [String : String]()
+                
+                tempDictionary["address"] = location[10] as? String
+                tempDictionary["name"] = location[17] as? String
+                tempDictionary["type"] = location[18] as? String
+                tempDictionary["waterfront"] = location[19] as? String
+                tempDictionary["zip"] = location[13] as? String
+                tempDictionary["coordinates"] = location[8] as? String
+                
+                self.parsedDictionary[(location[17] as? String)!] = tempDictionary
+                
+            }
+            
+            print(self.parsedDictionary["TLC Sculpture Park Garden"]!["address"])
+            
+        }
+        
+    }
+
+    
+    
     
     lazy var applicationDocumentsDirectory: NSURL = {
         // The directory the application uses to store the Core Data store file. This code uses a directory named "com.kencooke.Team_Tiger" in the application's documents Application Support directory.
