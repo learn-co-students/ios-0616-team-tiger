32f6241c4d81cd7930f98e6c25a5b810f6369225
TiciaChance <laticia.chance@flatironschool.com>
2016-08-10 09:52:39 -0400

    all the parsed data from the main APIs we want to use

--- Team-Tiger/Team-Tiger/DataStore.swift
+++ Team-Tiger/Team-Tiger/DataStore.swift
@@ -15,7 +15,6 @@ class DataStore {
     var masterParksDictionary = [String : [String : String]]()
     
     
-    
     //static makes it a singleton
     static let store = DataStore()
     var user = [User]()
@@ -23,9 +22,6 @@ class DataStore {
     func fetchData() {
         
         let userFetchRequest = NSFetchRequest(entityName: "User")
-        //execute fetch request
-        //storing fetch request
-        
         do {
             user = try managedObjectContext.executeFetchRequest(userFetchRequest) as! [User]
         } catch {
@@ -70,7 +66,7 @@ class DataStore {
                 self.masterParksDictionary[(location[17] as? String)!] = tempDictionary as Dictionary
                 
             }
-    
+            
         }
         
     }
@@ -111,70 +107,185 @@ class DataStore {
     }
     
     
-    
-    
-    lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.kencooke.Team_Tiger" in the application's documents Application Support directory.
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
-        return urls[urls.count-1]
-    }()
-    
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+// FUNCTIONS FOR FARMERS MARKET + BOTANICAL GARDENS + COMMUNITY GARDEN PARSED DATA
+    func farmersMarketParse() {
         
-        let modelURL = NSBundle.mainBundle().URLForResource("Team_Tiger", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
-    }()
-    
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Team_Tiger.sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
-            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
-        } catch {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+        var farmersMarketArray: [[String:String]] = []
+        
+        Alamofire.request(.GET, "https://data.cityofnewyork.us/api/views/j8gx-kc43/rows.json?") .responseJSON { response in
+            self.FMdictionary = response.result.value as! NSDictionary
             
-            dict[NSUnderlyingErrorKey] = error as NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-            abort()
-        }
-        
-        return coordinator
-    }()
-    
-    lazy var managedObjectContext: NSManagedObjectContext = {
-        // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
-        let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
-        managedObjectContext.persistentStoreCoordinator = coordinator
-        return managedObjectContext
-    }()
-    
-    // MARK: - Core Data Saving support
-    
-    func saveContext () {
-        if managedObjectContext.hasChanges {
-            do {
-                try managedObjectContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
-                abort()
+            if let jsonData = response.data {
+                let jsonObj = JSON(data: jsonData)
+                
+                let arrayOfData = jsonObj["data"].array
+                
+                var dictionaryWithInfo = [String:String]()
+                
+                if let arrayOfData = arrayOfData {
+                    
+                    for detail in arrayOfData {
+                        
+                        dictionaryWithInfo["name"] = detail[8].string
+                        dictionaryWithInfo["zip"] = detail[13].string
+                        dictionaryWithInfo["longitude"] = detail[15].string
+                        dictionaryWithInfo["latitude"] = detail[14].string
+                        
+                        if let addressInDictionary = detail[10].string {
+                            
+                            dictionaryWithInfo["address"] = addressInDictionary
+                            print(dictionaryWithInfo)
+                            
+                        } else {
+                            print("IN SEARCH OF ADDRESS")
+                        }
+                        
+                        farmersMarketArray.append(dictionaryWithInfo)
+                    }
+                    print(dictionaryWithInfo)
+                }
+                
             }
         }
     }
     
+    
+    
+    func botanicalGardensParse() {
+        
+        
+        var botanicalGardenArray: [[String: String]] = []
+        
+        
+        Alamofire.request(.GET, "https://data.cityofnewyork.us/api/views/69ih-b9ki/rows.json?accessType=DOWNLOAD") .responseJSON {
+            response in
+            
+            self.botanicalGardenDict = response.result.value as! NSDictionary
+            
+            if let JSONdata = response.data {
+                let jsonObject = JSON(data: JSONdata)
+                
+                let arrayofData = jsonObject["data"].array
+                
+                
+                for object in arrayofData! {
+                    
+                    var botanicalGardenDictionary = [String:String]()
+                    botanicalGardenDictionary["Botanical Garden"] = object[9].string
+                    botanicalGardenDictionary["Phone Number"] = object[10].string
+                    botanicalGardenDictionary["Website"] = object[11].string
+                    botanicalGardenDictionary["Address"] = object[12].string
+                    botanicalGardenDictionary["Borough"] = object[14].string
+                    
+                    botanicalGardenArray.append(botanicalGardenDictionary)
+                    
+                }
+            }
+            print("\n\n\n\nTHIS IS OUTSIDE THE LOOP \(botanicalGardenArray)")
+        }
+    }
+    
+    func greenThumbParse() {
+        
+        var greenThumbArray: [[String: String]] = []
+        
+        Alamofire.request(.GET, "https://data.cityofnewyork.us/api/views/ajxm-kzmj/rows.json?") .responseJSON { response in
+            self.greenThumbdictionary = response.result.value as! NSDictionary
+            
+            if let jsonData = response.data {
+                let jsonObj = JSON(data: jsonData)
+                
+                let arrayOfData = jsonObj["data"].array
+                
+                var dictionaryWithInfo = [String:String]()
+                
+                if let arrayOfData = arrayOfData {
+                    
+                    for detail in arrayOfData {
+                        
+                        dictionaryWithInfo["Garden"] = detail[12].string
+                        dictionaryWithInfo["Address"] = detail[13].string
+                        //dictionaryWithInfo["Cross Streets"] = detail[17].string
+                        
+                        if let neighborhoodInDictionary = detail[16].string {
+                            
+                            dictionaryWithInfo["neighborhood"] = neighborhoodInDictionary
+                            
+                        } else {
+                            print("\n\n\n NEIGHBORHOOD ISN'T PROVIDED\n\n\n")
+                        }
+                        
+                        greenThumbArray.append(dictionaryWithInfo)
+                    }
+                    
+                    print("THIS IS OUTSIDE LOOP \(dictionaryWithInfo)")
+                }
+                
+            }
+        }
+    }
 }
 
+lazy var applicationDocumentsDirectory: NSURL = {
+    // The directory the application uses to store the Core Data store file. This code uses a directory named "com.kencooke.Team_Tiger" in the application's documents Application Support directory.
+    let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+    return urls[urls.count-1]
+}()
 
+lazy var managedObjectModel: NSManagedObjectModel = {
+    // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+    
+    let modelURL = NSBundle.mainBundle().URLForResource("Team_Tiger", withExtension: "momd")!
+    return NSManagedObjectModel(contentsOfURL: modelURL)!
+}()
+
+lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+    // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+    // Create the coordinator and store
+    let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+    let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("Team_Tiger.sqlite")
+    var failureReason = "There was an error creating or loading the application's saved data."
+    do {
+        try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+    } catch {
+        // Report any error we got.
+        var dict = [String: AnyObject]()
+        dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
+        dict[NSLocalizedFailureReasonErrorKey] = failureReason
+        
+        dict[NSUnderlyingErrorKey] = error as NSError
+        let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+        // Replace this with code to handle the error appropriately.
+        // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+        NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+        abort()
+    }
+    
+    return coordinator
+}()
+
+lazy var managedObjectContext: NSManagedObjectContext = {
+    // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
+    let coordinator = self.persistentStoreCoordinator
+    var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+    managedObjectContext.persistentStoreCoordinator = coordinator
+    return managedObjectContext
+}()
+
+// MARK: - Core Data Saving support
+
+func saveContext () {
+    if managedObjectContext.hasChanges {
+        do {
+            try managedObjectContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+    }
+}
+
+}
