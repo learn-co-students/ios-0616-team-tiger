14bcf1202238eecdc34b4131301a65807187f1b6
kenpcjr <kennethcooke@me.com>
2016-08-18 13:37:13 -0400

    Adds completion handler to AQI call

---
+++ Team-Tiger/Team-Tiger/AQIViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AQIViewController.swift
+//  Team-Tiger
+//
+//  Created by Kenneth Cooke on 8/18/16.
+//  Copyright © 2016 kencooke. All rights reserved.
+//
+
+import UIKit
+
+class AQIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
